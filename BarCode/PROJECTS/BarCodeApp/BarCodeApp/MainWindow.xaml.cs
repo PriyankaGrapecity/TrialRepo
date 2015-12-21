3494e7c71db28731cbdd9a4eeb8cea43cebc497a
PriyankaGrapecity <priyanka.gupta@grapecity.com>
2015-12-21 14:28:00 +0530

    Changes

---
+++ BarCode/PROJECTS/BarCodeApp/BarCodeApp/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//#region Namespace
+using C1.BarCode;
+//#endregion
+
+namespace BarCodeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //#region SubscribeEvents
+            this.Loaded += MainWindow_Loaded;
+            cbCodeType.SelectionChanged += cbCodeType_SelectionChanged;
+            text.TextChanged += text_TextChanged;
+            //#endregion
+        }
+
+        //#region TextChangeEvent
+        void text_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(text.Text) &&
+                text.Text.Equals("http://www.componentone.com") &&
+                barcode.CodeType == CodeType.QRCode)
+                image.Opacity = 1;
+            else
+                image.Opacity = 0;
+        }
+        //#endregion
+
+        //#region ComboBoxSelectionChangeEvent
+        void cbCodeType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (barcode != null)
+            {
+                try
+                {
+                    barcode.CodeType = (CodeType)cbCodeType.SelectedItem;
+                    if (barcode.CodeType != CodeType.QRCode
+                        || !text.Text.Equals("http://www.componentone.com"))
+                    {
+                        image.Opacity = 1;
+                    }
+                    else
+                    {
+                        image.Opacity = 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        //#endregion
+
+        //#region MainWindowLoadEvent
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            cbCodeType.ItemsSource = Enum.GetValues(typeof(CodeType));
+            cbCodeType.SelectedItem = barcode.CodeType;
+        }
+        //#endregion
+
+    }    
+      
+}
+  
---
+++ BarCode/PROJECTS/BarCodeApp_VB/BarCodeApp_VB/MainWindow.xaml.vb
@@ -0,0 +1,70 @@
+﻿
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+'#region "Namespace"
+Imports C1.BarCode
+'#endregion
+
+Namespace BarCodeApp_VB
+
+    Partial Public Class MainWindow
+        Inherits Window
+        Public Sub New()
+            InitializeComponent()
+            '#region "SubscribeEvents"
+            AddHandler Me.Loaded, AddressOf MainWindow_Loaded
+            AddHandler cbCodeType.SelectionChanged, AddressOf cbCodeType_SelectionChanged
+            AddHandler text.TextChanged, AddressOf text_TextChanged
+            '#endregion
+        End Sub
+
+        '#region "TextChangeEvent"
+        Private Sub text_TextChanged(sender As Object, e As TextChangedEventArgs)
+            If Not String.IsNullOrEmpty(Text.Text) AndAlso Text.Text.Equals("http://www.componentone.com") AndAlso barcode.CodeType = CodeType.QRCode Then
+                Image.Opacity = 1
+            Else
+                Image.Opacity = 0
+            End If
+        End Sub
+        '#endregion
+
+        '#region "ComboBoxSelectionChangeEvent"
+        Private Sub cbCodeType_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+            If barcode IsNot Nothing Then
+                Try
+                    barcode.CodeType = DirectCast(cbCodeType.SelectedItem, CodeType)
+                    If barcode.CodeType <> CodeType.QRCode OrElse Not Text.Text.Equals("http://www.componentone.com") Then
+                        Image.Opacity = 1
+                    Else
+                        Image.Opacity = 0
+                    End If
+                Catch ex As Exception
+                    MessageBox.Show(ex.Message)
+                End Try
+            End If
+        End Sub
+        '#endregion
+
+        '#region "MainWindowLoadEvent"
+        Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs)
+            cbCodeType.ItemsSource = [Enum].GetValues(GetType(CodeType))
+            cbCodeType.SelectedItem = barcode.CodeType
+        End Sub
+        '#endregion
+    End Class
+
+End Namespace
+
+
+
